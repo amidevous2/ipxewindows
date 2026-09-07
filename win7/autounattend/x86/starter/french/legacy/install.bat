ca9c93c5c8be560c822f2a7d7c5e8a75301c2a59
amidevous2 <amidevous+2@gmail.com>
2026-09-07 12:21:35 +0200

    Create install.bat

---
+++ win7/autounattend/x86/starter/french/legacy/install.bat
@@ -0,0 +1,108 @@
+@echo off
+wpeinit
+set giturl=https://github.com/amidevous2/ipxewindows
+set gitbcommit=main
+set winversion=windows7
+set winversionmin=w7
+set isoversionmin=win7
+set arch1=x86
+set arch2=x86
+set cygmirror=https://mirrors.kernel.org/sourceware/cygwin-archive/20221123/
+if exist x:\Windows\System32\e1d68x64.inf pnputil /i /a x:\Windows\System32\e1d68x64.inf
+if exist x:\Windows\System32\balloon.inf pnputil /i /a x:\Windows\System32\balloon.inf
+if exist x:\Windows\System32\netkvm.inf pnputil /i /a x:\Windows\System32\netkvm.inf
+if exist x:\Windows\System32\viostor.inf pnputil /i /a x:\Windows\System32\viostor.inf
+if exist x:\Windows\System32\VBoxGuest.inf pnputil /i /a x:\Windows\System32\VBoxGuest.inf
+if exist x:\Windows\System32\VBoxGuestEarlyNT.inf pnputil /i /a x:\Windows\System32\VBoxGuestEarlyNT.inf
+if exist x:\Windows\System32\VBoxMouse.inf pnputil /i /a x:\Windows\System32\VBoxMouse.inf
+if exist x:\Windows\System32\VBoxVideo.inf pnputil /i /a x:\Windows\System32\VBoxVideo.inf
+if exist x:\Windows\System32\VBoxVideoEarlyNT.inf pnputil /i /a x:\Windows\System32\VBoxVideoEarlyNT.inf
+if exist x:\Windows\System32\VBoxWddm.inf pnputil /i /a x:\Windows\System32\VBoxWddm.inf
+if exist x:\Windows\System32\fwcfg.inf pnputil /i /a x:\Windows\System32\fwcfg.inf
+if exist x:\Windows\System32\pvpanic-pci.inf pnputil /i /a x:\Windows\System32\pvpanic-pci.inf
+if exist x:\Windows\System32\pvpanic.inf pnputil /i /a x:\Windows\System32\pvpanic.inf
+if exist x:\Windows\System32\qemupciserial.inf pnputil /i /a x:\Windows\System32\qemupciserial.inf
+if exist x:\Windows\System32\qxldod.inf pnputil /i /a x:\Windows\System32\qxldod.inf
+if exist x:\Windows\System32\viofs.inf pnputil /i /a x:\Windows\System32\vioprot.inf
+if exist x:\Windows\System32\viofs.inf pnputil /i /a x:\Windows\System32\viofs.inf
+if exist x:\Windows\System32\viogpudo.inf pnputil /i /a x:\Windows\System32\viogpudo.inf
+if exist x:\Windows\System32\vioinput.inf pnputil /i /a x:\Windows\System32\vioinput.inf
+if exist x:\Windows\System32\viorng.inf pnputil /i /a x:\Windows\System32\viorng.inf
+if exist x:\Windows\System32\vioser.inf pnputil /i /a x:\Windows\System32\vioser.inf
+setlocal enableDelayedExpansion
+for /f "delims=" %%a in ('ipconfig /all') do (
+    set line=%%a
+    if not "!line:~0,1!"==" " if not "!line:adapter=!"=="!line!" (
+        set name=!line:*adapter =!
+        set name=!name::=!
+    )
+    for /f "tokens=1,2,*" %%b in ("%%a") do (
+        if "%%b %%c"=="Physical Address." (
+            set mac=%%d
+            set mac=!mac:*: =!
+            echo !name!: !mac!
+            if "!mac!"=="52-54-00-01-13-9E" (
+                netsh interface ipv4 set address name="Ethernet" static 163.172.118.206 mask=255.255.255.255 gateway=62.210.202.1 10
+            )
+            else (
+                ipconfig /renew
+            )
+        )
+    )
+)
+net start dnscache
+netsh interface ipv4 set dns name="Ethernet" static 8.8.8.8 primary
+netsh interface ipv4 set winsservers name="Ethernet" static 8.8.8.8
+ipconfig /flushdns﻿
+wpeutil WaitForNetwork
+echo 'select disk 0' > config.txt
+echo 'clean' >> config.txt
+echo 'convert mbr' >> config.txt
+echo 'create partition primary size=100' >> config.txt
+echo 'format fs=ntfs quick label="System"' >> config.txt
+echo 'active' >> config.txt
+echo 'create partition primary' >> config.txt
+echo 'format fs=ntfs quick label="Windows"' >> config.txt
+echo 'assign letter=C' >> config.txt
+echo 'shrink desired=10240' >> config.txt
+echo 'create partition primary size=10240' >> config.txt
+echo 'format fs=ntfs quick label="INSTALL"' >> config.txt
+echo 'assign letter=D' >> config.txt
+echo 'exit' >> config.txt
+
+diskpart /s config.txt
+
+ping -n 1 google.fr
+setup-%arch1%.exe --no-admin --root D:\Cygwin\ --quiet-mode --no-shortcuts --no-startmenu --allow-unsupported-windows --arch %arch1% --force-current --no-desktop --no-replaceonreboot --no-verify --no-version-check --no-warn-deprecated-windows --no-write-registry --only-site --site %cygmirror% -l S:\Cygwin\var\cache\apt\packages --packages dos2unix,wget,ca-certificates
+D:\Cygwin\bin\wget.exe %giturl%/releases/download/%winversion%/%isoversionmin%min%arch1%.iso -O D:\%isoversionmin%min%arch1%.iso
+7z x -y D:\%isoversionmin%min%arch1%.iso -oD:\
+D:\Cygwin\bin\wget.exe %giturl%/releases/download/%winversion%/install.swm -O D:\sources\install.swm
+D:\Cygwin\bin\wget.exe %giturl%/releases/download/%winversion%/install2.swm -O D:\sources\install2.swm
+D:\Cygwin\bin\wget.exe %giturl%/releases/download/%winversion%/install3.swm -O D:\sources\install3.swm
+D:\Cygwin\bin\wget.exe %giturl%/releases/download/%winversion%/install4.swm -O D:\sources\install4.swm
+D:\Cygwin\bin\wget.exe %giturl%/releases/download/%winversion%/install5.swm -O D:\sources\install5.swm
+D:\Cygwin\bin\wget.exe %giturl%/raw/refs/heads/main/win7/autounattend/x86/starter/french/legacy/autounattend.xml -O D:\autounattend.xml
+D:\sources\setup.exe /unattend:"D:\autounattend.xml" /noreboot
+if %gitbcommit%==win10 Dism /Image:C:\ /enable-feature /featurename:NetFx3 /All /Source:"D:\sources\sxs" /LimitAccess /NoRestart /LogLevel:4
+if exist x:\Windows\System32\e1d68x64.inf Dism /Image:C:\ /Add-Driver /Driver:X:\Windows\System32\e1d68x64.inf
+if exist x:\Windows\System32\balloon.inf Dism /Image:C:\ /Add-Driver /Driver:X:\Windows\System32\balloon.inf
+if exist x:\Windows\System32\netkvm.inf Dism /Image:C:\ /Add-Driver /Driver:X:\Windows\System32\netkvm.inf
+if exist x:\Windows\System32\viostor.inf Dism /Image:C:\ /Add-Driver /Driver:X:\Windows\System32\viostor.inf
+if exist x:\Windows\System32\VBoxGuest.inf Dism /Image:C:\ /Add-Driver /Driver:X:\Windows\System32\VBoxGuest.inf
+if exist x:\Windows\System32\VBoxGuestEarlyNT.inf Dism /Image:C:\ /Add-Driver /Driver:X:\Windows\System32\VBoxGuestEarlyNT.inf
+if exist x:\Windows\System32\VBoxMouse.inf Dism /Image:C:\ /Add-Driver /Driver:X:\Windows\System32\VBoxMouse.inf
+if exist x:\Windows\System32\VBoxVideo.inf Dism /Image:C:\ /Add-Driver /Driver:X:\Windows\System32\VBoxVideo.inf
+if exist x:\Windows\System32\VBoxVideoEarlyNT.inf Dism /Image:C:\ /Add-Driver /Driver:X:\Windows\System32\VBoxVideoEarlyNT.inf
+if exist x:\Windows\System32\VBoxWddm.inf.inf Dism /Image:C:\ /Add-Driver /Driver:X:\Windows\System32\VBoxWddm.inf
+if exist x:\Windows\System32\fwcfg.inf Dism /Image:C:\ /Add-Driver /Driver:X:\Windows\System32\fwcfg.inf
+if exist x:\Windows\System32\pvpanic-pci.inf Dism /Image:C:\ /Add-Driver /Driver:X:\Windows\System32\pvpanic-pci.inf
+if exist x:\Windows\System32\pvpanic.inf Dism /Image:C:\ /Add-Driver /Driver:X:\Windows\System32\pvpanic.inf
+if exist x:\Windows\System32\qemupciserial.inf Dism /Image:C:\ /Add-Driver /Driver:X:\Windows\System32\qemupciserial.inf
+if exist x:\Windows\System32\qxldod.inf Dism /Image:C:\ /Add-Driver /Driver:X:\Windows\System32\qxldod.inf
+if exist x:\Windows\System32\vioprot.inf Dism /Image:C:\ /Add-Driver /Driver:X:\Windows\System32\vioprot.inf
+if exist x:\Windows\System32\viofs.inf Dism /Image:C:\ /Add-Driver /Driver:X:\Windows\System32\viofs.inf
+if exist x:\Windows\System32\viogpudo.inf Dism /Image:C:\ /Add-Driver /Driver:X:\Windows\System32\viogpudo.inf
+if exist x:\Windows\System32\vioinput.inf Dism /Image:C:\ /Add-Driver /Driver:X:\Windows\System32\vioinput.inf
+if exist x:\Windows\System32\viorng.inf Dism /Image:C:\ /Add-Driver /Driver:X:\Windows\System32\viorng.inf
+if exist x:\Windows\System32\vioser.inf Dism /Image:C:\ /Add-Driver /Driver:X:\Windows\System32\vioser.inf
+shutdown -r -t 1
