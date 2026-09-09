45298fdd479b25883b3545c0fcb78e8a315570eb
amidevous2 <amidevous+2@gmail.com>
2026-09-09 21:50:30 +0200

    Update install.bat

--- win7/autounattend/x86/starter/french/legacy/install.bat
+++ win7/autounattend/x86/starter/french/legacy/install.bat
@@ -121,21 +121,21 @@ rem mkdir S:\Cygwin\var\cache\apt\packages
 rem setup-%arch1%.exe --no-admin --root S:\Cygwin\ --quiet-mode --no-shortcuts --no-startmenu --allow-unsupported-windows --arch %arch1% --force-current --no-desktop --no-replaceonreboot --no-verify --no-version-check --no-warn-deprecated-windows --no-write-registry --only-site --site %cygmirror% -l S:\Cygwin\var\cache\apt\packages --packages dos2unix,wget,ca-certificates
 rem ping 127.0.0.1 -n 31 >nul
 rem S:\Cygwin\bin\wget.exe %giturl%/releases/download/%winversion%/%isoversionmin%min%arch1%.iso -O S:\%isoversionmin%min%arch1%.iso
-wget %giturl%/releases/download/%winversion%/%isoversionmin%min%arch1%.iso -O S:\%isoversionmin%min%arch1%.iso
+wget --no-check-certificate %giturl%/releases/download/%winversion%/%isoversionmin%min%arch1%.iso -O S:\%isoversionmin%min%arch1%.iso
 rem ping 127.0.0.1 -n 31 >nul
 7z x -y S:\%isoversionmin%min%arch1%.iso -oS:\
 rem ping 127.0.0.1 -n 31 >nul
-wget %giturl%/releases/download/%winversion%/install.swm -O S:\sources\install.swm
+wget --no-check-certificate %giturl%/releases/download/%winversion%/install.swm -O S:\sources\install.swm
 rem ping 127.0.0.1 -n 31 >nul
-wget %giturl%/releases/download/%winversion%/install2.swm -O S:\sources\install2.swm
+wget --no-check-certificate %giturl%/releases/download/%winversion%/install2.swm -O S:\sources\install2.swm
 rem ping 127.0.0.1 -n 31 >nul
-wget %giturl%/releases/download/%winversion%/install3.swm -O S:\sources\install3.swm
+wget --no-check-certificate %giturl%/releases/download/%winversion%/install3.swm -O S:\sources\install3.swm
 rem ping 127.0.0.1 -n 31 >nul
-wget %giturl%/releases/download/%winversion%/install4.swm -O s:\sources\install4.swm
+wget --no-check-certificate %giturl%/releases/download/%winversion%/install4.swm -O s:\sources\install4.swm
 rem ping 127.0.0.1 -n 31 >nul
-wget %giturl%/releases/download/%winversion%/install5.swm -O S:\sources\install5.swm
+wget --no-check-certificate %giturl%/releases/download/%winversion%/install5.swm -O S:\sources\install5.swm
 rem ping 127.0.0.1 -n 31 >nul
-wget %giturl%/raw/refs/heads/main/win7/autounattend/x86/starter/french/legacy/autounattend.xml -O S:\autounattend.xml
+wget --no-check-certificate %giturl%/raw/refs/heads/main/win7/autounattend/x86/starter/french/legacy/autounattend.xml -O S:\autounattend.xml
 rem ping 127.0.0.1 -n 31 >nul
 S:\sources\setup.exe /unattend:"S:\autounattend.xml" /noreboot
 if %gitbcommit%==win10 Dism /Image:C:\ /enable-feature /featurename:NetFx3 /All /Source:"D:\sources\sxs" /LimitAccess /NoRestart /LogLevel:4
