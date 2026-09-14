c5d906502be9e670e8a5b87113db29aabfbf6d74
amidevous2 <amidevous+2@gmail.com>
2026-09-14 20:24:50 +0200

    Update SetupComplete.cmd

--- win7/autounattend/x86/starter/french/legacy/SetupComplete.cmd
+++ win7/autounattend/x86/starter/french/legacy/SetupComplete.cmd
@@ -7,23 +7,10 @@ DISM /Online /Add-Package /PackagePath:"%WINDIR%\Setup\Files\Windows6.1-KB253355
 %WINDIR%\Setup\Files\windows-kb890830-v5.145.exe /quiet
 %WINDIR%\Setup\Files\MAS_AIO.cmd /Z-Windows /S
 "%WINDIR%\Setup\Files\winrar-x32-701fr.exe" /S
-"C:\Program Files\7-Zip\7z.exe" x "%WINDIR%\Setup\Files\KexSetup_Debug_1_2_1_2229.exe"
-mkdir "C:\Program Files\VxKex"
-mkdir "C:\Program Files\Kex32"
-xcopy /E /I /Y "%WINDIR%\Setup\Files\Core" "C:\Program Files\VxKex"
-xcopy /E /I /Y "%WINDIR%\Setup\Files\Core32" "C:\Program Files\VxKex"
-xcopy /E /I /Y "%WINDIR%\Setup\Files\Kex32" "C:\Program Files\VxKex\Kex32"
-regsvr32.exe /s "C:\Program Files\VxKex\KexShlEx.dll"
-for /f "tokens=2*" %%a in ('reg query "HKLM\System\CurrentControlSet\Control\Session Manager\Environment" /v Path') do reg add "HKLM\System\CurrentControlSet\Control\Session Manager\Environment" /v Path /t REG_EXPAND_SZ /d "%%b;C:\Program Files\VxKex" /f
-REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\Firefox Setup 155.0.1.exe" /v "KExEnabled" /t REG_DWORD /d 1 /f
-REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\Firefox Setup 155.0.1.exe" /v "KExSubsystemVersion" /t REG_DWORD /d 60003 /f
-REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\Firefox Setup 155.0.1.exe" /v "KExReportOSVersion" /t REG_DWORD /d 100 /f
-REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\Firefox Setup 155.0.1.exe" /v "VerifierDlls" /t REG_SZ /d "KExDll.dll" /f
+"%WINDIR%\Setup\Files\KexSetup_Release_1_2_1_2229.exe" /SILENTUNATTEND
+"C:\Program Files\VxKex\KexCfg.exe" /EXE:"%WINDIR%\Setup\Files\Firefox Setup 155.0.1.exe" /ENABLE:1 /WINVERSPOOF:WIN10
 "%WINDIR%\Setup\Files\Firefox Setup 155.0.1.exe" /S
-REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\firefox.exe" /v "KExEnabled" /t REG_DWORD /d 1 /f
-REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\firefox.exe" /v "KExSubsystemVersion" /t REG_DWORD /d 60003 /f
-REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\firefox.exe" /v "KExReportOSVersion" /t REG_DWORD /d 100 /f
-REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\firefox.exe" /v "VerifierDlls" /t REG_SZ /d "KExDll.dll" /f
+"C:\Program Files\VxKex\KexCfg.exe" /EXE:"C:\Program Files\Mozilla Firefox\firefox.exe" /ENABLE:1 /WINVERSPOOF:WIN10
 REG ADD "HKLM\SOFTWARE\Policies\Mozilla\Firefox" /v "DisableAppUpdate" /t REG_DWORD /d 1 /f
 del /q /f "%~f0"
 rd /q /s "%WINDIR%\Setup\Files"
