50a9a41df17a34c2efdb8e989c1dcb04b14c687f
amidevous2 <amidevous+2@gmail.com>
2026-09-14 22:21:55 +0200

    Update SetupComplete.cmd

--- win7/autounattend/x86/starter/french/legacy/SetupComplete.cmd
+++ win7/autounattend/x86/starter/french/legacy/SetupComplete.cmd
@@ -8,9 +8,18 @@ DISM /Online /Add-Package /PackagePath:"%WINDIR%\Setup\Files\Windows6.1-KB253355
 %WINDIR%\Setup\Files\MAS_AIO.cmd /Z-Windows /S
 "%WINDIR%\Setup\Files\winrar-x32-701fr.exe" /S
 "%WINDIR%\Setup\Files\KexSetup_Release_1_2_1_2229.exe" /SILENTUNATTEND
-"C:\Program Files\VxKex\KexCfg.exe" /EXE:"%WINDIR%\Setup\Files\Firefox Setup 155.0.1.exe" /ENABLE:1 /WINVERSPOOF:WIN10
-"%WINDIR%\Setup\Files\Firefox Setup 155.0.1.exe" /S
-"C:\Program Files\VxKex\KexCfg.exe" /EXE:"C:\Program Files\Mozilla Firefox\firefox.exe" /ENABLE:1 /WINVERSPOOF:WIN10
+rem "C:\Program Files\VxKex\KexCfg.exe" /EXE:"%WINDIR%\Setup\Files\Firefox Setup 155.0.1.exe" /ENABLE:1 /WINVERSPOOF:WIN10
+"%WINDIR%\Setup\Files\Firefox Setup 115.40.0esr.exe" /S"
+rem "C:\Program Files\VxKex\KexCfg.exe" /EXE:"C:\Program Files\Mozilla Firefox\firefox.exe" /ENABLE:1 /WINVERSPOOF:WIN10
+rem "C:\Program Files\VxKex\KexCfg.exe" /EXE:"C:\Program Files\Mozilla Firefox\crashhelper.exe" /ENABLE:1 /WINVERSPOOF:WIN10
+rem "C:\Program Files\VxKex\KexCfg.exe" /EXE:"C:\Program Files\Mozilla Firefox\crashreporter.exe" /ENABLE:1 /WINVERSPOOF:WIN10
+rem "C:\Program Files\VxKex\KexCfg.exe" /EXE:"C:\Program Files\Mozilla Firefox\maintenanceservice.exe" /ENABLE:1 /WINVERSPOOF:WIN10
+rem "C:\Program Files\VxKex\KexCfg.exe" /EXE:"C:\Program Files\Mozilla Firefox\maintenanceservice_installer.exe" /ENABLE:1 /WINVERSPOOF:WIN10
+rem "C:\Program Files\VxKex\KexCfg.exe" /EXE:"C:\Program Files\Mozilla Firefox\nmhproxy.exe" /ENABLE:1 /WINVERSPOOF:WIN10
+rem "C:\Program Files\VxKex\KexCfg.exe" /EXE:"C:\Program Files\Mozilla Firefox\pingsender.exe" /ENABLE:1 /WINVERSPOOF:WIN10
+rem "C:\Program Files\VxKex\KexCfg.exe" /EXE:"C:\Program Files\Mozilla Firefox\plugin-container.exe" /ENABLE:1 /WINVERSPOOF:WIN10
+rem "C:\Program Files\VxKex\KexCfg.exe" /EXE:"C:\Program Files\Mozilla Firefox\private_browsing.exe" /ENABLE:1 /WINVERSPOOF:WIN10
+rem "C:\Program Files\VxKex\KexCfg.exe" /EXE:"C:\Program Files\Mozilla Firefox\updater.exe" /ENABLE:1 /WINVERSPOOF:WIN10
 REG ADD "HKLM\SOFTWARE\Policies\Mozilla\Firefox" /v "DisableAppUpdate" /t REG_DWORD /d 1 /f
 del /q /f "%~f0"
 rd /q /s "%WINDIR%\Setup\Files"
