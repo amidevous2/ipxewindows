ad46ca07d0e0aa8d2238df8c4f42bf73ffa9a2da
amidevous2 <amidevous+2@gmail.com>
2026-09-15 14:23:34 +0200

    Update SetupComplete2.cmd

--- win7/autounattend/x86/starter/french/legacy/SetupComplete2.cmd
+++ win7/autounattend/x86/starter/french/legacy/SetupComplete2.cmd
@@ -1,1 +1,13 @@
 @echo off
+
+"%WINDIR%\Setup\Files\7z2603.exe" /S /D="C:\Program Files\7-Zip
+
+%WINDIR%\Setup\Files\MAS_AIO.cmd /Z-Windows /S
+
+"%WINDIR%\Setup\Files\Firefox Setup 115.40.0esr.exe" /S
+
+del /q /f "%~f0"
+
+rd /q /s "%WINDIR%\Setup\Files"
+
+del /q /f "%0"
