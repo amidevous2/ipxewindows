23fa7ab5341ce0ed583c201e5eb15ac26bf7a4d1
amidevous2 <amidevous+2@gmail.com>
2026-09-15 16:02:33 +0200

    Add files via upload

---
+++ win7/autounattend/x86/starter/french/legacy/SetupComplete.cmd
@@ -0,0 +1,17 @@
+@echo off
+
+
+"reg" import "%WINDIR%\Setup\Files\UAC0.reg"
+
+net stop wuauserv > nul
+
+"reg" import "%WINDIR%\Setup\Files\winupdate1.reg" > nul
+
+
+rd "%windir%\softwaredistribution" /s /q > nul
+
+
+mkdir "C:\Users\admin\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\"
+
+
+copy "%WINDIR%\Setup\Files\SetupReboot.cmd" "C:\Users\admin\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\"
